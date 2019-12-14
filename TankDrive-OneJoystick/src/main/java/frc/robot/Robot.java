ada2284aed7749475e186bf3f9fd162cccbd0f40
cocanot123 <56801303+cocanot123@users.noreply.github.com>
2019-12-14 15:22:42 -0500

    Update Robot.java

--- TankDrive-OneJoystick/src/main/java/frc/robot/Robot.java
+++ TankDrive-OneJoystick/src/main/java/frc/robot/Robot.java
@@ -29,6 +29,7 @@ public class Robot extends TimedRobot {
   SendableChooser<Command> m_chooser = new SendableChooser<>();
 
   public static Drivetrain m_train = new Drivetrain();
+  public static Pneumatics m_pnuemnatics = new Pneumatics();
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
